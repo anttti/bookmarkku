0f49cd14e399b73f958b0e008ac3fa721d743d9a
Antti Mattila <antti.mattila@futurice.com>
2023-03-19 19:45:06 +0200

    chore: add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Markku.Repo.insert!(%Markku.Bookmarks.Bookmark{
+  title: "The Verge",
+  url: "https://www.theverge.com",
+  description:
+    "The Verge is about technology and how it makes us feel. Founded in 2011, we offer our audience everything from breaking news to reviews to award-winning features and investigations, on our site, in video, and in podcasts.",
+  unread: true
+})
