c6c332dfec645045a37c2f0bb57cdbed7e7e19f3
Antti Mattila <antti.mattila@futurice.com>
2023-03-19 19:45:19 +0200

    feat: add migration tools

--- lib/markku/release.ex
+++ lib/markku/release.ex
@@ -18,6 +18,43 @@ defmodule Markku.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  @doc """
+  Print the migration status for configured Repos' migrations.
+  """
+  def migration_status do
+    for repo <- repos(), do: print_migrations_for(repo)
+  end
+
+  defp print_migrations_for(repo) do
+    paths = repo_migrations_path(repo)
+
+    {:ok, repo_status, _} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.migrations(&1, paths), mode: :temporary)
+
+    IO.puts(
+      """
+      Repo: #{inspect(repo)}
+        Status    Migration ID    Migration Name
+      --------------------------------------------------
+      """ <>
+        Enum.map_join(repo_status, "\n", fn {status, number, description} ->
+          "  #{pad(status, 10)}#{pad(number, 16)}#{description}"
+        end) <> "\n"
+    )
+  end
+
+  defp repo_migrations_path(repo) do
+    config = repo.config()
+    priv = config[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
+    config |> Keyword.fetch!(:otp_app) |> Application.app_dir() |> Path.join(priv)
+  end
+
+  defp pad(content, pad) do
+    content
+    |> to_string
+    |> String.pad_trailing(pad)
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
